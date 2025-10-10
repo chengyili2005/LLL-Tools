5e9fec99aa0fba670faad5b9c95a12d2dbf1982a
chengyi <chengli200513@gmail.com>
2025-10-09 20:22:56 -0700

    Tons of updates

---
+++ praat_concat_tier.py
@@ -0,0 +1,34 @@
+from textgrid import TextGrid, IntervalTier
+import os
+import sys
+import pandas as pd
+
+# Main Script
+if __name__ == "__main__":
+
+    # Check correct usage
+    if len(sys.argv) != 4:
+        print("Usage: python3 praat_concat_tier.py [og_textgrid.TextGrid] [new_tier.txt] [output_dir]")
+        sys.exit(1)
+
+    # Grab paths
+    og_textgrid_path = sys.argv[1]
+    new_tier_path = sys.argv[2]
+    output_dir = sys.argv[3]
+
+    # Read files
+    tg = TextGrid()
+    tg.read(og_textgrid_path)
+    new_tier_df = pd.read_csv(new_tier_path, sep='\t')
+
+    # Create a new tier & append
+    tier_name = new_tier_df.loc[0, 'tier']
+    new_tier = IntervalTier(name=tier_name, minTime=tg.minTime, maxTime=tg.maxTime)
+    for _, row in new_tier_df.iterrows():
+        new_tier.add(row['tmin'], row['tmax'], row['text'])
+    tg.append(new_tier)
+
+    # Write file
+    output_file = os.path.join(output_dir, os.path.basename(og_textgrid_path))
+    tg.write(output_file)
+    print(f"Updated TextGrid saved to: {output_file}")
--- whisper_transcribe.py
+++ whisper_transcribe.py
@@ -1,7 +1,7 @@
 # Simple command line interface for bulk transcribing with whisper into transcript files
 # Usage: python3 whisper_transcribe.py [input_directory] [output_directory]
-# Description: 
-# - This script opens the [input_directory] and grabs all supported files (.mp3) and converts them into transcripts.
+# Description:
+# - This script opens the [input_directory] and grabs all supported files (.wav) and converts them into transcripts.
 # - Transcripts are formatted the same as Praat's single tier text exports.
 import os
 import sys
@@ -12,7 +12,7 @@ import numpy as np
 if __name__ == "__main__":
 
 	# Ensure correct usage
-	if (len(sys.argv) != 3): 
+	if (len(sys.argv) != 3):
 		print("Correct usage: python3 whisper_transcribe.py [input_directory] [output_directory]")
 		quit()
 	input_dir = sys.argv[1]
@@ -22,31 +22,47 @@ if __name__ == "__main__":
 	model = WhisperModel("small")
 
 	# Iterate through all files in the input directory
-	supported_type = ".mp3" # For now, we'll only support .mp3, but we can support anything that whisper supports
+	supported_type = ".wav" # CHANGE FILE TYPE IF NEEDED
 	print("Opening: " + input_dir)
 	for root, dirs, files in os.walk(input_dir):
 		for file_name in files:
-			if supported_type in file_name and not file_name.startswith("."): 
+			if supported_type in file_name and not file_name.startswith("."):
 				curr_path = os.path.join(root, file_name)
-				segments, info = model.transcribe(curr_path, word_timestamps=True) # Future direction: This has a prompt parameter for better transcriptions.
+				segments, info = model.transcribe(curr_path, task="transcribe", word_timestamps=True, language="es") # NOTE: For handling code switching have the input language be set to the non-dominant language! (Usually Spanish for our case)
 				segments = list(segments)
-				
-				# Load transcriptions into a file
-				transcription = []
+
+				# Load transcription_words into a file
+				transcription_words = []
+				transcription_utterances = []
 				for segment in segments:
+					transcription_utterances.append({
+						"tmin": segment.start,
+						"tier": "FromWhisper - Utterances",
+						"text": segment.text,
+						"tmax": segment.end
+					})
 					for word in segment.words:
-						transcription.append({
+						transcription_words.append({
 							"tmin": word.start,
 							"tier": "FromWhisper - Words",
 							"text": word.word,
 							"tmax": word.end
 						})
-				
-				# Write transcription
-				output_file = output_dir + os.path.splitext(os.path.basename(file_name))[0] + ".txt" if output_dir.endswith("/") else output_dir + "/" + os.path.splitext(os.path.basename(file_name))[0] + ".txt"
-				with open(output_file, "w", encoding="utf-8") as outfile:
+
+				# Write transcription_words
+				output_words = output_dir + os.path.splitext(os.path.basename(file_name))[0] + "_words.txt" if output_dir.endswith("/") else output_dir + "/" + os.path.splitext(os.path.basename(file_name))[0] + "_words.txt"
+				with open(output_words, "w", encoding="utf-8") as outfile:
 					outfile.write("tmin\ttier\ttext\ttmax\n")
-					for row in transcription:
+					for row in transcription_words:
 						outfile.write(f"{row['tmin']}\t{row['tier']}\t{row['text']}\t{row['tmax']}\n")
-					print("Finished: " + output_file)
+					print("Finished: " + output_words)
+
+				# Write transcription_utterances
+				output_utterances = output_dir + os.path.splitext(os.path.basename(file_name))[0] + "_utterances.txt" if output_dir.endswith("/") else output_dir + "/" + os.path.splitext(os.path.basename(file_name))[0] + "_utterances.txt"
+				with open(output_utterances, "w", encoding="utf-8") as outfile:
+					outfile.write("tmin\ttier\ttext\ttmax\n")
+					for row in transcription_utterances:
+						outfile.write(f"{row['tmin']}\t{row['tier']}\t{row['text']}\t{row['tmax']}\n")
+					print("Finished: " + output_utterances)
+
 	print("Script done")
